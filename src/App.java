4e66c8029a9d2d05240cc97d33d11d0278c5d01a
Daiskz <gabriel.matsubara@gmail.com>
2022-10-16 20:07:42 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Digite a temperatura em graus celsius: ");
+            double celsius = entrada.nextDouble();
+            double fahrenheit = ((celsius * 1.8) + 32);
+            System.out.print("A temperatura em fahrenheit é: " + fahrenheit);
+        }
+    }
+}
